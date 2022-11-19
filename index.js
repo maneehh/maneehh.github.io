c572b363efb971022fb50a81808b3b277abcd051
maneehh <117098781+maneehh@users.noreply.github.com>
2022-11-19 22:23:15 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,113 @@
+//1
+let a1 = 14
+let b1 = 5
+console.log(a1 + b1)
+console.log(a1 - b1)
+console.log(a1*b1)
+console.log(a1/b1)
+console.log(a1 % b1)
+
+
+let a2 = 99
+let b2 = 11
+console.log(a2 + b2)
+console.log(a2 - b2)
+console.log(a2*b2)
+console.log(a2/b2)
+console.log(a2 % b2)
+
+
+let a3 = 81
+let b3 = 16
+console.log(a3 + b3)
+console.log(a3 - b3)
+console.log(a3*b3)
+console.log(a3/b3)
+console.log(a3 % b3)
+
+
+
+//2
+let name = 'Mane';
+let age = 18;
+console.log( name = ' My name is ' + name + ',', age = 'I am ' + age);
+
+
+
+//3
+let number1 = 0
+if (number1 < 0) {
+    console.log("Yes");
+}
+else {
+     console.log("No");
+}
+
+let number2 = -71
+if (number2 < 0) {
+    console.log("Yes");
+}
+else {
+     console.log("No");
+}
+
+let number3 = 89
+if (number3 < 0) {
+    console.log("Yes");
+}
+else {
+     console.log("No");
+}
+
+
+//4
+let number1 = 123
+if(number1 % 2 == 0) {
+    console.log("even");
+}
+else {
+    console.log("odd");
+}
+
+let number2 = 35
+if(number2 % 2 == 0) {
+    console.log("even");
+}
+else {
+    console.log("odd");
+}
+
+let number3 = 70
+if(number3 % 2 == 0) {
+    console.log("even");
+}
+else {
+    console.log("odd");
+}
+
+
+
+//5
+let a = 3
+let b = 14
+if (b % a == 0) {
+  console.log(1);
+}else {
+  console.log(0);
+}
+
+let a1 = 18
+let b1 = 2
+if (a1 % b1 == 0) {
+  console.log(1);
+}else {
+  console.log(0);
+}
+
+let a3= 7
+let b3= 21
+if (b3% a3== 0) {
+  console.log(1);
+}else {
+  console.log(0);
+}
